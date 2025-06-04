767617844664628c152c29d9920d43b4ac11d91f
Daisuke <daisuke.kida@nijibox.co.jp>
2025-06-04 14:32:28 +0900

    feat: layout

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from './header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        <Link href="/">Tsundcle</Link>
+      </h1>
+      <nav>
+        <ul className={styles.navItems}>
+          <li>
+            <Link className={styles.navLink} href="/">
+              本棚
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.navLink} href="/">
+              本を登録
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
